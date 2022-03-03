c6f00a3d7018c996ac493ac2a5316742b4ff8bf0
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2022-03-03 21:31:57 +0100

    detect to which db connect using dj_database_url

--- proj/settings.py
+++ proj/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import dj_database_url
 from pathlib import Path
 import os
 
@@ -77,10 +78,11 @@ WSGI_APPLICATION = "proj.wsgi.application"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
+    "default": dj_database_url.config()
 }
 
 
